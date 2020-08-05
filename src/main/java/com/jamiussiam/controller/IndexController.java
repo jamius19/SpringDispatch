23e841209ce9241abfc8277b47af9757a81c753d
Jamius Siam <jamiussiam@gmail.com>
2020-08-05 15:48:05 +0600

    Initial commit

---
+++ src/main/java/com/jamiussiam/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.jamiussiam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "Hello World");
+        return "index";
+    }
+}
